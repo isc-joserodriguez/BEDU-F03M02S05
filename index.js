39e53c7bc1cdc33d3492a344c2ab2bd8227ee1e4
Jose Rodriguez <isc.joserodriguez@gmail.com>
2021-05-24 22:24:30 -0600

    Ejemplo 4

--- index.js
+++ index.js
@@ -1,18 +1,15 @@
-/*
-Crear API (express)
-llamada /primos que regrese el listado de los primeros N primos
-*/
 const express = require('express');
-const pr = require('prime-functions')
 
 const app = express();
 app.get('/', function (request, response) {
+    console.log(request.get('Mi-encabezado-personalizado'));
     response.end('Hello BEDU');
 });
 
-app.get('/primos/:num', function (request, response) {
-    const { num = 1 } = request.params;
-    response.end(`Los primeros ${num} primos son:\n [${pr.firstNPrimes(num).join(' , ')}]`)
+app.get('/user-agent', function (request, response) {
+    const userAgent = request.get('User-Agent');
+    const who = request.get('Host');
+    response.end(`Consultado desde: ${userAgent} / ${who}`);
 })
 
 app.listen(8080, function () {
